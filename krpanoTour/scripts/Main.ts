40ca1f51f18f9832bac0de39991618f680182fff
rebeccachemata13 <80950540+rebeccachemata13@users.noreply.github.com>
2022-12-28 15:33:59 +0100

    Changing namespace and implement mushroom

--- krpanoTour/scripts/Main.ts
+++ krpanoTour/scripts/Main.ts
@@ -1,30 +1,32 @@
-console.log("MainScript running");
+namespace VRExplorer {
 
-let imageSpot: HTMLImageElement = <HTMLImageElement>document.getElementById("herbstwaldHotspotImg");
-let firstWeiterButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("first_weiter_button");
-let iconDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("infoIcon");
-let infoIcon: HTMLImageElement = <HTMLImageElement>document.createElement("img");
-let firstCommentBox: HTMLDivElement = <HTMLDivElement>document.getElementById("firstCommentBox");
-let marker: HTMLAnchorElement = <HTMLAnchorElement>document.getElementById("tour_link");
-console.log(marker);
-
-
-firstWeiterButton.addEventListener("click", firstHotspotClick);
+    console.log("MainScript running");
+    let firstWeiterButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("first_weiter_button");
+    let iconDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("infoIcon");
+    let infoIcon: HTMLImageElement = <HTMLImageElement>document.createElement("img");
+    let firstCommentBox: HTMLDivElement = <HTMLDivElement>document.getElementById("firstCommentBox");
+    let marker: HTMLAnchorElement = <HTMLAnchorElement>document.getElementById("tour_link");
+    console.log(marker);
 
 
-function firstHotspotClick(): void {
-    marker.classList.remove("disabled");
-    firstCommentBox.classList.add("invisibleDisabled");
-    infoIcon.src = "../ui/UI_icon_help.png";
-    infoIcon.classList.add("infoIcon");
-    infoIcon.addEventListener("click", enableComment);
-    iconDiv.appendChild(infoIcon);
-    console.log(infoIcon);
-}
+    firstWeiterButton.addEventListener("click", firstHotspotClick);
 
-function enableComment(): void {
-    marker.classList.add("disabled");
-    firstCommentBox.classList.remove("invisibleDisabled");
-    infoIcon.remove();
+
+    function firstHotspotClick(): void {
+        marker.classList.remove("disabled");
+        firstCommentBox.classList.add("invisibleDisabled");
+        infoIcon.src = "../ui/UI_icon_help.png";
+        infoIcon.classList.add("infoIcon");
+        infoIcon.addEventListener("click", enableComment);
+        iconDiv.appendChild(infoIcon);
+        console.log(infoIcon);
+    }
+
+    function enableComment(): void {
+        marker.classList.add("disabled");
+        firstCommentBox.classList.remove("invisibleDisabled");
+        infoIcon.remove();
+    }
+
 }
 
